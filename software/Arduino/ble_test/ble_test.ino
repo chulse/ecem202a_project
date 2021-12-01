eef1cfddcf9d69680ec80efe65db37b6dc24c8e7
chulse <39817852+chulse@users.noreply.github.com>
2021-11-30 23:08:35 -0800

    Initial BLE arduino code

---
+++ software/Arduino/ble_test/ble_test.ino
@@ -0,0 +1,65 @@
+/*
+  Arduino Nano 33 BLE Getting Started
+  BLE peripheral with a simple Hello World greeting service that can be viewed
+  on a mobile phone
+  Adapted from Arduino BatteryMonitor example
+*/
+
+#include <ArduinoBLE.h>
+
+static const char* greeting = "Hello World!";
+
+BLEService greetingService("180C");  // User defined service
+
+BLEStringCharacteristic greetingCharacteristic("2A56",  // standard 16-bit characteristic UUID
+    BLERead, 13); // remote clients will only be able to read this
+
+BLEShortCharacteristic classifierCharacteristic("2A57", BLERead | BLENotify);
+
+void setup() {
+  Serial.begin(9600);    // initialize serial communication
+  while (!Serial);
+
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin
+
+  if (!BLE.begin()) {   // initialize BLE
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("Nano33BLE_ECE202");  // Set name for connection
+  BLE.setAdvertisedService(greetingService); // Advertise service
+  greetingService.addCharacteristic(greetingCharacteristic); // Add characteristic to service
+  greetingService.addCharacteristic(classifierCharacteristic); // Add characteristic to service
+  BLE.addService(greetingService); // Add service
+  greetingCharacteristic.setValue(greeting); // Set greeting string
+  classifierCharacteristic.setValue(123); // Set greeting string
+
+  BLE.advertise();  // Start advertising
+  Serial.print("Peripheral device MAC: ");
+  Serial.println(BLE.address());
+  Serial.println("Waiting for connections...");
+}
+
+void loop() {
+  BLEDevice central = BLE.central();  // Wait for a BLE central to connect
+
+  // if a central is connected to the peripheral:
+  if (central) {
+    Serial.print("Connected to central MAC: ");
+    // print the central's BT address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    while (central.connected()){
+      delay(1000);
+      classifierCharacteristic.setValue(millis()); //this number can be set to whatever we detect happening and then received on the RPI.
+      } // keep looping while connected
+    
+    // when the central disconnects, turn off the LED:
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Disconnected from central MAC: ");
+    Serial.println(central.address());
+  }
+}

6b3138051762b4ac6b587f08fd0c321a9ffbd545
Chester <chester.h4@gmail.com>
2021-11-30 22:47:03 -0800

    Initial working PI code

---
+++ software/Pi/testingBLE.py
@@ -0,0 +1,58 @@
+ADDRESS = (
+    "91:0C:EC:15:4A:FE"
+)
+"""
+Service Explorer
+----------------
+An example showing how to access and print out the services, characteristics and
+descriptors of a connected GATT server.
+Created on 2019-03-25 by hbldh <henrik.blidh@nedomkull.com>
+"""
+
+import sys
+import platform
+import asyncio
+import logging
+
+from bleak import BleakClient
+
+logger = logging.getLogger(__name__)
+
+
+async def main(address):
+    async with BleakClient(address) as client:
+        logger.info(f"Connected: {client.is_connected}")
+
+        for service in client.services:
+            logger.info(f"[Service] {service}")
+            for char in service.characteristics:
+                if "read" in char.properties:
+                    try:
+                        value = bytes(await client.read_gatt_char(char.uuid))
+                        logger.info(
+                            f"\t[Characteristic] {char} ({','.join(char.properties)}), Value: {value}"
+                        )
+                    except Exception as e:
+                        logger.error(
+                            f"\t[Characteristic] {char} ({','.join(char.properties)}), Value: {e}"
+                        )
+
+                else:
+                    value = None
+                    logger.info(
+                        f"\t[Characteristic] {char} ({','.join(char.properties)}), Value: {value}"
+                    )
+
+                for descriptor in char.descriptors:
+                    try:
+                        value = bytes(
+                            await client.read_gatt_descriptor(descriptor.handle)
+                        )
+                        logger.info(f"\t\t[Descriptor] {descriptor}) | Value: {value}")
+                    except Exception as e:
+                        logger.error(f"\t\t[Descriptor] {descriptor}) | Value: {e}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main(sys.argv[1] if len(sys.argv) == 2 else ADDRESS))

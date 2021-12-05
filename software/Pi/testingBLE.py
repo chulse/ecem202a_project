4c4ca25b4ab9dbc94f5a197634e6e4b5874c62fc
Chester <39817852+chulse@users.noreply.github.com>
2021-12-05 14:16:11 -0800

    Testing IFTTTTTTTTT

--- software/Pi/testingBLE.py
+++ software/Pi/testingBLE.py
@@ -1,31 +1,25 @@
-ADDRESS = (
-    "91:0C:EC:15:4A:FE"
-)
-
-
-# -*- coding: utf-8 -*-
-"""
-Notifications
--------------
-Example showing how to add notifications to a characteristic and handle the responses.
-Updated on 2019-07-03 by hbldh <henrik.blidh@gmail.com>
-"""
-
 import sys
 import asyncio
 import platform
+import time
+import requests
 
 from bleak import BleakClient
 
+EVENTS = ["Dishwasher", "Oven", "Hairdryer"]
 
 # you can change these to match your device or override them from the command line
 CHARACTERISTIC_UUID = "00002a57-0000-1000-8000-00805f9b34fb"
 
-
+ADDRESS = (
+    "91:0C:EC:15:4A:FE"
+)
 
 def notification_handler(sender, data):
     """Simple notification handler which prints the data received."""
-    print("{0}: {1}".format(sender, int.from_bytes(bytes=data,byteorder="little", signed=False)))
+    event_number = int.from_bytes(bytes=data,byteorder="little", signed=False)
+    print("{0}: {1}".format(sender, event_number))
+    r = requests.post(f"https://maker.ifttt.com/trigger/RPI_EVENT/with/key/czpWqSQT7vXx8EvUU5Qt-x", params={"value1":f"appliance_name ... {event_number}","value2":"duraiton","value3":"normal/avg/threshold"})
 
 
 async def main(address, char_uuid):
@@ -33,7 +27,7 @@ async def main(address, char_uuid):
         print(f"Connected: {client.is_connected}")
 
         await client.start_notify(char_uuid, notification_handler)
-        await asyncio.sleep(5.0)
+        await asyncio.sleep(60.0)
         await client.stop_notify(char_uuid)
 
 
